ef23d7d876012529526ca166c31c7f63c5ef52e7
songmingu(mac) <min9nim@gmail.com>
2018-11-05 13:07:58 +0900

    운영/개발환경 구분

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
+str=`git branch | grep "*"`
+echo "Current branch : " "$str"
+
 npm run build
-gcloud config set project sharelink-nextjs
+
+if [ "$str" == "* master" ];then
+    gcloud config set project sharelink-nextjs
+else
+    gcloud config set project sharelink-dev
+fi
+
 gcloud app deploy --quiet
+
+
