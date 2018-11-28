62cce9f7ee4cd478f78b06ab699ab4d6ac690050
songmingu(mac) <min9nim@gmail.com>
2018-11-28 13:33:35 +0900

    테스트코드 작성

---
+++ test/com/pure.test.js
@@ -0,0 +1,149 @@
+//let { _findLink } = require("../../com/pure.js");
+import {_findLink} from "../../com/pure.js";
+
+let links = [
+    {
+        "author": {
+            "id": "BXvaL6OAN",
+            "name": "민구"
+        },
+        "like": [],
+        "read": [],
+        "toread": [],
+        "comments": [
+            {
+                "author": {
+                    "id": "BXvaL6OAN",
+                    "name": "민구"
+                },
+                "_id": "5bfbc07f1c984f0006bc2ce5",
+                "id": "ZhuiFk5MD",
+                "linkID": "2T8eLA25_",
+                "comment": "재미있겠다",
+                "updatedAt": 1543225471451,
+                "createdAt": 1543225471451
+            }
+        ],
+        "url": "https://medium.com/@jooyunghan/%EA%B0%9C%EB%AF%B8-%EC%88%98%EC%97%B4-%EC%B1%85%EC%9D%84-%EC%93%B0%EB%8B%A4-31f42dbe4d31",
+        "id": "2T8eLA25_",
+        "createdAt": 1543225455293,
+        "updatedAt": 1543225463246,
+        "title": "책: 개미 수열을 푸는 10가지 방법 – Jooyung Han (한주영) – Medium",
+        "image": "",
+        "desc": "제목 그대로다. 개미 수열 문제의 풀이를 설명하는 것으로 책을 썼다. (육아 휴직의 힘!)",
+        "refLinks": []
+    },
+    {
+        "author": {
+            "id": "BXvaL6OAN",
+            "name": "민구"
+        },
+        "like": [],
+        "read": [],
+        "toread": [
+            "BXvaL6OAN"
+        ],
+        "comments": [],
+        "url": "https://rhostem.github.io/posts/2017-09-07-redux-saga-toast-control/",
+        "id": "UYQnZ9ItL",
+        "createdAt": 1543221786280,
+        "updatedAt": 1543224610058,
+        "title": "redux-saga를 활용한 Redux 비동기 액션 처리",
+        "image": "",
+        "desc": "사이드 이펙트를 효율적으로 관리할 수 있는 Redux 미들웨어 Redux-saga. 토스트 메시지를 표시하고 제거하는 과정을 Redux-saga를 사용해 간단히 구현한 예제를 제공",
+        "refLinks": []
+    },
+    {
+        "author": {
+            "id": "BXvaL6OAN",
+            "name": "민구"
+        },
+        "like": [],
+        "read": [],
+        "toread": [],
+        "comments": [],
+        "url": "http://hacks.mozilla.or.kr/2015/08/es6-in-depth-generators/",
+        "id": "YJwIiF8c7",
+        "createdAt": 1543222502641,
+        "updatedAt": 1543222561854,
+        "title": "  ES6 In Depth: 제너레이터(Generator) ★\n      Mozilla 웹 기술 블로그  ",
+        "image": "",
+        "desc": "",
+        "refLinks": [
+            {
+                "author": {
+                    "id": "BXvaL6OAN",
+                    "name": "민구"
+                },
+                "like": [],
+                "read": [],
+                "toread": [],
+                "comments": [],
+                "linkID": "YJwIiF8c7",
+                "url": "https://medium.com/@jooyunghan/%EC%9E%90%EB%B0%94%EC%8A%A4%ED%81%AC%EB%A6%BD%ED%8A%B8-%EC%A0%9C%EB%84%88%EB%A0%88%EC%9D%B4%ED%84%B0%EC%9D%98-%EC%9E%AC%EB%AF%B8-246553cadfbd",
+                "id": "R2NV8Rt5G",
+                "createdAt": 1543222544627,
+                "updatedAt": 1543222934498,
+                "title": "자바스크립트 제너레이터의 재미 – Jooyung Han (한주영) – Medium",
+                "image": "",
+                "desc": "JavaScript의 Generator는 재미있는 녀석이다. 함수이면서 함수와는 다른 동작 방식 때문에 잘만 사용한다면 의외로 쓸모가 많을 녀석이다."
+            },
+            {
+                "author": {
+                    "id": "BXvaL6OAN",
+                    "name": "민구"
+                },
+                "like": [],
+                "read": [],
+                "toread": [],
+                "comments": [
+                    {
+                        "author": {
+                            "id": "BXvaL6OAN",
+                            "name": "민구"
+                        },
+                        "_id": "5bfcd825dcbc8e0006dbdba7",
+                        "id": "YxV3ziPxs",
+                        "linkID": "966lAVnKB",
+                        "comment": "이거 내용이 꾀 어렵네",
+                        "updatedAt": 1543297061855,
+                        "createdAt": 1543297061855
+                    }
+                ],
+                "linkID": "YJwIiF8c7",
+                "url": "https://edykim.com/ko/post/introducing-javascript-generator-and-koa.js/",
+                "id": "966lAVnKB",
+                "createdAt": 1543222614961,
+                "updatedAt": 1543222614961,
+                "title": "JavaScript의 Generator와 Koa.js 소개",
+                "image": "",
+                "desc": "사이드 프로젝트에서 Express를 오랜 기간 사용했었는데 hapi 가 좋다는 얘기를 듣고는 hapi를 많이 사용해왔다. Hapi도 단순하긴 하지만 “설정만 넣으면 되는” 단순함이라서 설정에 들어가는 수고가 꽤 컸다. 최근에는 토이 프로젝트에서 API를 작성하는데 에러 발생 여부에 따라서 {&quot;ok&quot;: true} 하나 넣어주는 작업에 오만가지 코드를 작성해야 했다. express와 다르게 미들웨어에서 request, response에 접근할 수 있는 포인트가 워낙에 많아 더 복잡하게 느껴졌다. 그러던 중 예전에 잠시..."
+            },
+            {
+                "author": {
+                    "id": "BXvaL6OAN",
+                    "name": "민구"
+                },
+                "like": [],
+                "read": [],
+                "toread": [],
+                "comments": [],
+                "linkID": "YJwIiF8c7",
+                "url": "https://gist.github.com/marocchino/841e2ff62f59f420f9d9",
+                "id": "XZA1rBHHe",
+                "createdAt": 1543222656485,
+                "updatedAt": 1543222656485,
+                "title": "ES6시대의 JavaScript",
+                "image": "https://avatars0.githubusercontent.com/u/128431?s=400&v=4",
+                "desc": "ES6시대의 JavaScript. GitHub Gist: instantly share code, notes, and snippets."
+            }
+        ]
+    }
+]
+
+
+test('_findLink 테스트', () => {
+    expect(_findLink(links, "966lAVnKB").id).toBe("966lAVnKB");
+    expect(_findLink(links, "UYQnZ9ItL").id).toBe("UYQnZ9ItL");
+    expect(_findLink(links, "R2NV8Rt5G").id).toBe("R2NV8Rt5G");
+});
