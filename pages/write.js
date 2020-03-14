b5f8649294e235278c0b1f8175059142829f6360
min9nim <min9nim@gmail.com>
2020-03-15 08:44:51 +0900

    update

--- pages/write.js
+++ pages/write.js
@@ -1,53 +1,58 @@
-import Layout from '../comps/Layout.js';
+import Layout from '../comps/Layout.js'
 import { withRouter } from 'next/router'
-import app from "../src/app";
-import { observable, reaction, decorate } from "mobx";
-import shortid from "shortid";
-import $m from "../com/util.js";
-import "./write.scss";
-import { _findLink } from '../com/pure.js';
+import app from '../src/app'
+import { observable, reaction, decorate } from 'mobx'
+import shortid from 'shortid'
+import $m from '../com/util.js'
+import './write.scss'
+import { _findLink } from '../com/pure.js'
 
 class Write extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
 
-    app.state.userID = props.user.id;
-    app.user = props.user;
+    app.state.userID = props.user.id
+    app.user = props.user
 
-    let link = this.props.link;
-    link = Object.assign({}, link);   // 복사본을 전달
+    let link = this.props.link
+    link = Object.assign({}, link) // 복사본을 전달
 
-    this.state = link.id ? link : {
-      id: "",
-      url: "",
-      title: "",
-      desc: "",
-      image: "",
-      like: [],
-      read: [],
-      toread: [],
-      author: {
-        id: app.user.id,
-        name: app.user.name
-      }
-    };
+    this.state = link.id
+      ? link
+      : {
+          id: '',
+          url: '',
+          title: '',
+          desc: '',
+          image: '',
+          like: [],
+          read: [],
+          toread: [],
+          author: {
+            id: app.user.id,
+            name: app.user.name,
+          },
+        }
 
-    app.view.Write = this;
+    app.view.Write = this
 
     // mobx 설정
-    decorate(this, { state: observable });
-    reaction(() => JSON.stringify(this.state), () => {
-      this.forceUpdate();
-    });
+    decorate(this, { state: observable })
+    reaction(
+      () => JSON.stringify(this.state),
+      () => {
+        this.forceUpdate()
+      },
+    )
   }
 
   static async getInitialProps({ req, asPath, query }) {
-    let user = await app.getUser(req);
-    app.user.token = user.token;
+    let user = await app.getUser(req)
+    app.user.token = user.token
 
-    let link;
+    let link
     if (req) {
-      let fetchRes = await app.api.fetchLink(req.query.id);
+      let fetchRes = await app.api.fetchLink(req.query.id)
       link = fetchRes[0]
     } else {
       //link = app.state.links.find(l => l.id === query.id);
@@ -57,34 +62,32 @@ class Write extends React.Component {
     return {
       menuIdx: 0,
       link,
-      user
+      user,
     }
   }
 
-
   cancel() {
-    this.props.router.push("/")
+    this.props.router.push('/')
   }
 
   componentWillUnmount() {
-    this._ismounted = false;
+    this._ismounted = false
   }
 
   async save() {
     if (!this.state.url) {
-      alert("링크를 입력해 주세요");
-      this.urlInput.focus();
-      return;
+      alert('링크를 입력해 주세요')
+      this.urlInput.focus()
+      return
     }
     if (!this.state.title) {
-      alert("제목을 입력해 주세요");
-      this.titleInput.focus();
-      return;
+      alert('제목을 입력해 주세요')
+      this.titleInput.focus()
+      return
     }
 
     // 인증을 위한 토큰 전달
-    this.state.token = app.user.token;
-
+    this.state.token = app.user.token
 
     function avoidXSS(link) {
       return Object.assign({}, link, {
@@ -98,83 +101,84 @@ class Write extends React.Component {
          */
         title: link.title,
         desc: link.desc,
-        image: $m.removeTag(link.image)
-      });
+        image: $m.removeTag(link.image),
+      })
     }
 
     if (this.state.id) {
       // 수정할 때
-      await app.api.putLink(avoidXSS(this.state));
+      await app.api.putLink(avoidXSS(this.state))
     } else {
       // 신규등록
-      let newLink = Object.assign({}, this.state, { id: shortid.generate() });
-      await app.api.postLink(avoidXSS(newLink));
+      let newLink = Object.assign({}, this.state, { id: shortid.generate() })
+      await app.api.postLink(avoidXSS(newLink))
     }
 
-    this.props.router.push("/");
-
+    this.props.router.push('/')
   }
 
   componentDidMount() {
-    this._ismounted = true;
+    this._ismounted = true
 
-    if (this.state.url === "") {
-      this.urlInput.focus();
+    if (this.state.url === '') {
+      this.urlInput.focus()
     }
 
     if (!app.auth.isLogin()) {
-      alert("글등록은 로그인이 필요합니다");
+      alert('글등록은 로그인이 필요합니다')
       //this.props.router.push("/login");
-      location.href = "/login";
+      location.href = '/login'
     }
   }
 
   handleChange(e) {
-    this.state[e.target.id] = e.target.value;
+    this.state[e.target.id] = e.target.value
   }
 
   async handleBlur() {
-    const { url, title, desc, image } = this.state;
+    const { url, title, desc, image } = this.state
 
-    if (url === "") return;
-    if (title && desc && image) return;
+    if (url === '') return
+    if (title && desc && image) return
 
-    const loadingMessage = "Loading.."
+    const loadingMessage = 'Loading..'
 
     // this.state.title = loadingMessage;
     // this.state.desc = loadingMessage;
     // this.state.image = loadingMessage;
 
-    this.titleInput.setAttribute("placeholder", loadingMessage);
-    this.descInput.setAttribute("placeholder", loadingMessage);
-    this.imageInput.setAttribute("placeholder", loadingMessage);
+    this.titleInput.setAttribute('placeholder', loadingMessage)
+    this.descInput.setAttribute('placeholder', loadingMessage)
+    this.imageInput.setAttribute('placeholder', loadingMessage)
 
     try {
-      const { title, image, desc } = await fetch('https://webscrap.min1.now.sh/webscrap', {
-    method: 'POST', 
-    headers:{
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      url: this.state.url
-    }),
-  }
-).then(res => res.json())
+      const { title, image, desc } = await fetch(
+        'https://webscrap.now.sh/webscrap',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            url: this.state.url,
+          }),
+        },
+      ).then(res => res.json())
 
       // let { title, image, desc } = await app.api.webscrap(this.state.url);
 
       // 타이틀 세팅
-      if (this.state.title === "") {
-        this.state.title = title;
+      if (this.state.title === '') {
+        this.state.title = title
       }
 
       // 설명세팅
-      if (this.state.desc === "") {
-        this.state.desc = desc;
+      if (this.state.desc === '') {
+        this.state.desc = desc
       }
 
       //이미지 세팅
-      this.state.image = image;
+      this.state.image = image
 
       // if (image && image.indexOf("http") === 0) {
       //   // http 로 시작하면 그냥 사용
@@ -185,75 +189,122 @@ class Write extends React.Component {
       //   //console.log(this.state.image);
       // }
 
-      if (this.state.title === "") {
-        this.titleInput.setAttribute("placeholder", "글 제목을 가져올 수 없습니다")
+      if (this.state.title === '') {
+        this.titleInput.setAttribute(
+          'placeholder',
+          '글 제목을 가져올 수 없습니다',
+        )
       }
-      if (this.state.desc === "") {
-        this.descInput.setAttribute("placeholder", "글 설명을 가져올 수 없습니다")
+      if (this.state.desc === '') {
+        this.descInput.setAttribute(
+          'placeholder',
+          '글 설명을 가져올 수 없습니다',
+        )
       }
-      if (this.state.image === "") {
-        this.imageInput.setAttribute("placeholder", "대표 이미지가 없습니다")
+      if (this.state.image === '') {
+        this.imageInput.setAttribute('placeholder', '대표 이미지가 없습니다')
       }
-
     } catch (e) {
-      console.error(e.message);
+      console.error(e.message)
     }
-
   }
 
   enterSave(e) {
-    if (e.key === "Enter") {
-      this.save();
+    if (e.key === 'Enter') {
+      this.save()
     }
   }
 
-
   enterCancel(e) {
-    if (e.key === "Enter") {
-      this.cancel();
+    if (e.key === 'Enter') {
+      this.cancel()
     }
   }
 
   initValue(e) {
-    this.state[e.target.parentNode.previousSibling.id] = ""
-    e.target.parentNode.previousSibling.focus();
+    this.state[e.target.parentNode.previousSibling.id] = ''
+    e.target.parentNode.previousSibling.focus()
   }
 
   render() {
     return (
       <Layout>
-        <div className="write-title">{this.state.id ? "내용 수정" : "링크 등록"}</div>
+        <div className="write-title">
+          {this.state.id ? '내용 수정' : '링크 등록'}
+        </div>
         <div className="wrapper">
           <div className="form">
             <div>
               <div className="label">글주소</div>
-              <input placeholder="http://" id="url" ref={el => { this.urlInput = el; }} value={this.state.url} onChange={this.handleChange.bind(this)} onBlur={this.handleBlur.bind(this)} />
+              <input
+                placeholder="http://"
+                id="url"
+                ref={el => {
+                  this.urlInput = el
+                }}
+                value={this.state.url}
+                onChange={this.handleChange.bind(this)}
+                onBlur={this.handleBlur.bind(this)}
+              />
               <div className="init-btn">
-                <i className="icon-cancel" onClick={this.initValue.bind(this)} />
+                <i
+                  className="icon-cancel"
+                  onClick={this.initValue.bind(this)}
+                />
               </div>
-
             </div>
             <div>
               <div className="label">글제목</div>
-              <input placeholder="" id="title" ref={el => { this.titleInput = el; }} value={this.state.title} onChange={this.handleChange.bind(this)} />
+              <input
+                placeholder=""
+                id="title"
+                ref={el => {
+                  this.titleInput = el
+                }}
+                value={this.state.title}
+                onChange={this.handleChange.bind(this)}
+              />
               <div className="init-btn">
-                <i className="icon-cancel" onClick={this.initValue.bind(this)} />
+                <i
+                  className="icon-cancel"
+                  onClick={this.initValue.bind(this)}
+                />
               </div>
-
             </div>
             <div>
               <div className="label">간단 설명(선택)</div>
-              <input placeholder="" id="desc" ref={el => { this.descInput = el; }} value={this.state.desc} onChange={this.handleChange.bind(this)} />
+              <input
+                placeholder=""
+                id="desc"
+                ref={el => {
+                  this.descInput = el
+                }}
+                value={this.state.desc}
+                onChange={this.handleChange.bind(this)}
+              />
               <div className="init-btn">
-                <i className="icon-cancel" onClick={this.initValue.bind(this)} />
+                <i
+                  className="icon-cancel"
+                  onClick={this.initValue.bind(this)}
+                />
               </div>
-
             </div>
             <div>
               <div className="label">대표 이미지 경로</div>
-              <input placeholder="" id="image" ref={el => { this.imageInput = el; }} value={this.state.image} onChange={this.handleChange.bind(this)} />
+              <input
+                placeholder=""
+                id="image"
+                ref={el => {
+                  this.imageInput = el
+                }}
+                value={this.state.image}
+                onChange={this.handleChange.bind(this)}
+              />
               <div className="init-btn">
-                <i className="icon-cancel" onClick={this.initValue.bind(this)} />
+                <i
+                  className="icon-cancel"
+                  onClick={this.initValue.bind(this)}
+                />
               </div>
             </div>
           </div>
@@ -263,8 +314,20 @@ class Write extends React.Component {
         </div>
 
         <div className="btn">
-          <div onClick={this.save.bind(this)} tabIndex="0" onKeyPress={this.enterSave.bind(this)}><i className="icon-floppy" /> 저장</div>
-          <div onClick={this.cancel.bind(this)} tabIndex="0" onKeyPress={this.enterCancel.bind(this)}><i className="icon-cancel" /> 취소</div>
+          <div
+            onClick={this.save.bind(this)}
+            tabIndex="0"
+            onKeyPress={this.enterSave.bind(this)}
+          >
+            <i className="icon-floppy" /> 저장
+          </div>
+          <div
+            onClick={this.cancel.bind(this)}
+            tabIndex="0"
+            onKeyPress={this.enterCancel.bind(this)}
+          >
+            <i className="icon-cancel" /> 취소
+          </div>
         </div>
       </Layout>
     )
