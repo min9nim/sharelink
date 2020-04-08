7473a6eb58b4fc96d521579f6373e044bbb8a691
min9nim <min9nim@gmail.com>
2020-04-08 10:20:36 +0900

    함수 컴포넌트로 변경

--- components/RefPostList.js
+++ components/RefPostList.js
@@ -1,21 +1,13 @@
-import React from "react";
-import RefPost from "./RefPost";
-import "./RefPostList.scss";
+import React from 'react'
+import RefPost from './RefPost'
+import './RefPostList.scss'
 
-
-export default class RefPostList extends React.Component {
-    constructor(props) {
-        // console.log("RefPostList 생성자 호출");
-        super(props);
-    }
-
-    render() {
-        return (
-            <ul className="RefPostList">
-                {this.props.refLinks.map(
-                    l => <RefPost key={l.id} link={l} />
-                )}
-            </ul>
-        );
-    }
+export default function RefPostList(props) {
+  return (
+    <ul className="RefPostList">
+      {props.refLinks.map((l) => (
+        <RefPost key={l.id} link={l} />
+      ))}
+    </ul>
+  )
 }
