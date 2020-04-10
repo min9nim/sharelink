9d81a9f1385759ed3c09c4e6b5bc712db2ce91f4
min9nim <min9nim@gmail.com>
2020-04-10 23:08:34 +0900

    fix build error

--- src/pages/like.js
+++ src/pages/like.js
@@ -1,6 +1,60 @@
-import Index from './index'
-// import { timelog } from '../com/util.js'
+import List from '../components/List'
+import Layout from '../components/Layout'
+import app from '../app'
+import { useState, useEffect } from 'react'
 
-export default function Like() {
-  return <Index />
+export default function Index(props) {
+  const logger = app.logger.addTags('Index')
+  logger.debug('start')
+  const [state, setState] = useState({ ...app.state, ...props.fetchRes })
+
+  useEffect(() => {
+    Object.assign(app.state, props.fetchRes)
+  }, [])
+
+  useEffect(() => {
+    logger.debug('[Index effect] 로긴여부 처리', app.state.links.length)
+    if (!props.user?.id) {
+      app.auth.signOut()
+      return
+    }
+    app.state.userID = props.user.id
+    app.user = props.user
+    global.sessionStorage?.setItem('user', JSON.stringify(app.user))
+  }, [])
+
+  useEffect(() => {
+    logger.debug('[Index effect] links 구독', app.state.links.length)
+    const subscription = app.linksSubject.subscribe((links) => {
+      logger.debug(
+        '[Index effect] links 구독 중 feed 받아 먹음',
+        links.length,
+        app.state.links.length,
+      )
+      setState({ ...app.state })
+    })
+    return () => {
+      subscription.unsubscribe()
+    }
+  }, [])
+
+  logger.debug('Index render')
+  return (
+    <Layout>
+      <List state={state} />
+    </Layout>
+  )
+}
+
+Index.getInitialProps = async ({ req, asPath }) => {
+  let menuIdx = app.state.menu.findIndex((m) => m.path === asPath)
+  let [user, fetchRes] = await Promise.all([
+    app.getUser(req),
+    app.api.fetchList({ menuIdx }),
+  ])
+  return {
+    menuIdx,
+    fetchRes,
+    user,
+  }
 }
--- src/pages/my.js
+++ src/pages/my.js
@@ -1,39 +1,52 @@
-import List from '../components/List.js'
+import List from '../components/List'
+import Layout from '../components/Layout'
 import app from '../app'
 import { useState, useEffect } from 'react'
 
-export default function My(props) {
+export default function Index(props) {
+  const logger = app.logger.addTags('Index')
+  logger.debug('start')
   const [state, setState] = useState({ ...app.state, ...props.fetchRes })
-  app.logger.debug(
-    'Index 렌더',
-    state.links.length,
-    props.fetchRes.links.length,
-  )
-  app.setState = setState
 
   useEffect(() => {
-    if (props.user?.id) {
-      app.state.userID = props.user.id
-      app.user = props.user
-      global.sessionStorage &&
-        global.sessionStorage.setItem('user', JSON.stringify(app.user))
-    } else {
-      if (global.document) {
-        // 클라이언트에서 실행시
-        app.auth.signOut()
-      }
+    Object.assign(app.state, props.fetchRes)
+  }, [])
+
+  useEffect(() => {
+    logger.debug('[Index effect] 로긴여부 처리', app.state.links.length)
+    if (!props.user?.id) {
+      app.auth.signOut()
+      return
     }
-  }, [props.user])
+    app.state.userID = props.user.id
+    app.user = props.user
+    global.sessionStorage?.setItem('user', JSON.stringify(app.user))
+  }, [])
 
   useEffect(() => {
-    app.logger.debug('My useEffect')
-    setState({ ...state, ...props.fetchRes })
-  }, [props.fetchRes])
+    logger.debug('[Index effect] links 구독', app.state.links.length)
+    const subscription = app.linksSubject.subscribe((links) => {
+      logger.debug(
+        '[Index effect] links 구독 중 feed 받아 먹음',
+        links.length,
+        app.state.links.length,
+      )
+      setState({ ...app.state })
+    })
+    return () => {
+      subscription.unsubscribe()
+    }
+  }, [])
 
-  return <List state={state} />
+  logger.debug('Index render')
+  return (
+    <Layout>
+      <List state={state} />
+    </Layout>
+  )
 }
 
-My.getInitialProps = async ({ req, asPath }) => {
+Index.getInitialProps = async ({ req, asPath }) => {
   let menuIdx = app.state.menu.findIndex((m) => m.path === asPath)
   let [user, fetchRes] = await Promise.all([
     app.getUser(req),
