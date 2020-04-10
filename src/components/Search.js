d0119ad3b22c13dd5d17ec39952b1ae2df89b4da
min9nim <min9nim@gmail.com>
2020-04-10 14:48:16 +0900

    convert to functional component

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,32 +3,21 @@ import { withRouter } from 'next/router'
 import './Search.scss'
 import { Observable, Subject } from 'rxjs'
 import { filter } from 'rxjs/operators'
+import { useState, useEffect } from 'react'
 
 const isAddMode = (event) =>
   event.target.value.indexOf('http') === 0 && app.auth.isLogin()
 
-class Search extends React.Component {
-  constructor(props) {
-    super(props)
+function Search(props) {
+  const [state, setState] = useState({
+    mode: 'search',
+    word: '',
+    subject: null,
+  })
 
-    this.state = {
-      mode: 'search',
-    }
-
-    app.view.Search = this
-  }
-
-  componentDidMount() {
-    this._ismounted = true
-    this.ipt_serarch.focus()
-
-    const observable = new Observable((subscriber) => {
-      this.subscriber = subscriber
-    })
-    const subject = new Subject()
-
-    this.subjectSubscription = observable.subscribe(subject)
-    this.keypressSubscription = subject
+  useEffect(() => {
+    state.subject = new Subject()
+    const keypressSubscription = state.subject
       .pipe(
         filter(
           ({ type, event }) =>
@@ -37,91 +26,74 @@ class Search extends React.Component {
       )
       .subscribe(({ event }) => {
         app.logger.debug('keypress 처리')
-        this.search(event.target.value)
+        search(event.target.value)
       })
-    this.changeSubscription = subject
+    const changeSubscription = state.subject
       .pipe(filter(({ type }) => type === 'change'))
       .subscribe(({ event }) => {
-        app.logger.debug('change 처리')
-        app.state.word = event.target.value
-        this.state.mode = isAddMode(event) ? 'add' : 'search'
+        app.logger.debug('change 처리', event.target.value)
+
+        setState({
+          ...state,
+          word: event.target.value,
+          mode: isAddMode(event) ? 'add' : 'search',
+        })
       })
-  }
+    const blurSubscription = state.subject
+      .pipe(filter(({ type, event }) => type === 'blur' && event.target.value))
+      .subscribe(({ event }) => {
+        app.logger.debug('blur 처리', event.target.value)
+        search(event.target.value)
+      })
+    return () => {
+      keypressSubscription.unsubscribe()
+      changeSubscription.unsubscribe()
+      blurSubscription.unsubscribe()
+    }
+  }, [state.subject])
 
-  componentWillUnmount() {
-    this._ismounted = false
-    this.subjectSubscription.unsubscribe()
-    this.keypressSubscription.unsubscribe()
-    this.changeSubscription.unsubscribe()
-  }
-
-  handleChange(e) {
-    this.subscriber.next({ type: 'change', event: e })
-  }
-
-  handleKeyPress(e) {
-    this.subscriber.next({ type: 'keypress', event: e })
-  }
-  handleBlur(e) {
-    if (!e.target.value) {
+  const search = async (word) => {
+    if (state.mode === 'search') {
+      await app.api.fetchList({
+        menuIdx: app.state.menuIdx,
+        idx: 0,
+        cnt: app.PAGEROWS,
+        word,
+      })
       return
     }
-    this.search(e.target.value)
-  }
-
-  search = async (word) => {
-    try {
-      if (this.state.mode === 'search') {
-        await app.api.fetchList({
-          menuIdx: app.state.menuIdx,
-          idx: 0,
-          cnt: app.PAGEROWS,
-          word,
-        })
-        return
-      }
-      if (!app.user.id) {
-        app.logger.warn((m) => console.log(...m('app.user.id is undefined')))
-        return
-      }
-
-      await app.api.postLink({
-        url: app.state.word,
-        author: {
-          id: app.user.id,
-          name: app.user.name,
-        },
-      })
-      this.state.mode = 'search'
-      app.state.word = ''
-    } catch (e) {
-      app.looger.error(e.message)
-      alert(e.message)
+    if (!app.user.id) {
+      app.logger.warn('app.user.id is undefined')
+      return
     }
+
+    await app.api.postLink({
+      url: state.word,
+      author: {
+        id: app.user.id,
+        name: app.user.name,
+      },
+    })
+    state.mode = 'search'
   }
 
-  render() {
-    console.log('Search 렌더링')
-    return (
-      <div className="ipt-wrapper">
-        {app.auth.isLogin() && this.state.mode === 'add' ? (
-          <i className="icon-doc-new" />
-        ) : (
-          <i className="icon-search" />
-        )}
-        <input
-          className="ipt-search"
-          ref={(el) => {
-            this.ipt_serarch = el
-          }}
-          value={app.state.word}
-          onChange={this.handleChange.bind(this)}
-          onKeyPress={this.handleKeyPress.bind(this)}
-          onBlur={this.handleBlur.bind(this)}
-        />
-      </div>
-    )
-  }
+  return (
+    <div className="ipt-wrapper">
+      {app.auth.isLogin() && state.mode === 'add' ? (
+        <i className="icon-doc-new" />
+      ) : (
+        <i className="icon-search" />
+      )}
+      <input
+        className="ipt-search"
+        autoFocus
+        value={state.word}
+        onChange={(e) => state.subject.next({ type: 'change', event: e })}
+        onKeyPress={(e) => state.subject.next({ type: 'keypress', event: e })}
+        onBlur={(e) => state.subject.next({ type: 'blur', event: e })}
+      />
+    </div>
+  )
 }
 
 export default withRouter(Search)
