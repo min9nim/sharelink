7e5163b76ccff98bbba0bb141f9ed744ca1f4457
min9nim <min9nim@gmail.com>
2020-06-09 09:32:34 +0900

    update

--- src/components/DarkMode.js
+++ src/components/DarkMode.js
@@ -6,34 +6,36 @@ import { moon, sun } from './atom/icon'
 export default function DarkMode() {
   return (
     <ThemeContext.Consumer>
-      {(ctx) => (
-        <Toggle
-          icons={{
-            checked: (
-              <img
-                src={moon}
-                alt="moon"
-                width="16"
-                height="16"
-                role="presentation"
-                style={{ pointerEvents: 'none' }}
-              />
-            ),
-            unchecked: (
-              <img
-                src={sun}
-                alt="sun"
-                width="16"
-                height="16"
-                role="presentation"
-                style={{ pointerEvents: 'none' }}
-              />
-            ),
-          }}
-          checked={ctx.theme === 'dark'}
-          onChange={(e) => ctx.setTheme(e.target.checked ? 'dark' : 'light')}
-        />
-      )}
+      {(ctx) =>
+        ctx.theme && (
+          <Toggle
+            icons={{
+              checked: (
+                <img
+                  src={moon}
+                  alt="moon"
+                  width="16"
+                  height="16"
+                  role="presentation"
+                  style={{ pointerEvents: 'none' }}
+                />
+              ),
+              unchecked: (
+                <img
+                  src={sun}
+                  alt="sun"
+                  width="16"
+                  height="16"
+                  role="presentation"
+                  style={{ pointerEvents: 'none' }}
+                />
+              ),
+            }}
+            checked={ctx.theme === 'dark'}
+            onChange={(e) => ctx.setTheme(e.target.checked ? 'dark' : 'light')}
+          />
+        )
+      }
     </ThemeContext.Consumer>
   )
 }
