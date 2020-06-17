12955e742849159f4ce41aacbeda000df558acf5
min9nim <min9nim@gmail.com>
2020-06-18 08:06:27 +0900

    webpack configuration

--- next.config.js
+++ next.config.js
@@ -11,4 +11,20 @@ module.exports = {
     API,
   },
   dir: './src',
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Note: we provide webpack above so you should not `require` it
+    // Perform customizations to webpack config
+    // Important: return the modified config
+    console.log(config)
+
+    // auto open window setting // do now works 😭
+    if(config.devServer){
+      config.devServer.open = true
+    }else{
+      config.devServer = {open: true}
+    }
+
+    return config
+  },
+
 }
